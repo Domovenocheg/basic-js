6a3682d446bba52040954303f1627499f4945e70
Yegor Dyakovskiy <yegor.dyakovskiy@aiesec.net>
2023-04-14 00:02:04 +0600

    feat: tried dream team test, it didnt work

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,14 +1,14 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
@@ -16,18 +16,23 @@ const { NotImplementedError } = require('../extensions/index.js');
 function createDreamTeam(members) {
   //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let arrTest = [];
   let str = "";
+  if (typeof members !== typeof arrTest && !null && !"") return false;
+
   for (let i = 0; i < members.length; i++) {
-    //console.log(members[i][0]);
-    str += members[i][0];
-    console.log(str);
+    if (typeof members[i] === typeof "") {
+      //console.log(members[i]);
+      str += members[i].replaceAll(" ", "")[0];
+    }
+    //console.log(members[i].replaceAll(" ", "")[0]);
+    //str += members[i].replaceAll(" ", "")[0];
+    //console.log(str.replaceAll(" ", ""));
   }
-  let result = str.split("").sort().join("");
-  console.log(result);
+  let result = str.toUpperCase().split("").sort().join("");
+  return result;
 }
 
-createDreamTeam(["Matt", "Ann", "Dmitry", "Max"]);
-
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
