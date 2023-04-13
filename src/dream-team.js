3d63b5a463f659d2dd5d5ee91446b635ee431bc1
Yegor Dyakovskiy <yegor.dyakovskiy@aiesec.net>
2023-04-13 23:27:18 +0600

    feat: add 1 part of count-cats

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let str = "";
+  for (let i = 0; i < members.length; i++) {
+    //console.log(members[i][0]);
+    str += members[i][0];
+    console.log(str);
+  }
+  let result = str.split("").sort().join("");
+  console.log(result);
 }
+
+createDreamTeam(["Matt", "Ann", "Dmitry", "Max"]);
 
 module.exports = {
   createDreamTeam
